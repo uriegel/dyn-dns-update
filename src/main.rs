2fe858776c98067bdc490f4ee8677954c06a48fd
Uwe Riegel <uriegel@hotmail.de>
2022-01-17 11:49:53 +0100

    settings from .config dir

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,13 @@ pub struct Settings {
 
 fn main() {
     println!("Starting dyn-dns-update...");    
-    let settings = fs::read_to_string("dyn-dns-update.conf").expect("Could not read settings");
+
+    let settings = 
+        fs::read_to_string(dirs::config_dir()
+                .expect("Could not find config dir")
+                .join("dyn-dns-update")
+                .join("dyn-dns-update.conf")
+            ).expect("Could not read settings");
     let settings: Settings = serde_json::from_str(&settings).expect("Could not extract settings");
     let log_settings = Settings {
         passwd: "***".to_string(),
