4e3c053e19a9edfbc7ea5089ce1a53610950c408
Uwe Riegel <uriegel@hotmail.de>
2022-01-17 16:15:35 +0100

    Check domains to refresh

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::{fs, net::IpAddr, str::FromStr};
+use std::{fs, net::IpAddr, str::FromStr, fmt};
 
 use dns_lookup::lookup_host;
 use serde::Deserialize;
 
-#[derive(Debug)]
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Settings {
@@ -11,6 +10,17 @@ pub struct Settings {
     pub provider: String,
     pub account: String,
     pub passwd: String,
+}
+
+impl fmt::Debug for Settings {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Settings")
+         .field("domains", &self.domains)
+         .field("provider", &self.provider)
+         .field("account", &self.account)
+         .field("passwd", &"***")
+         .finish()
+    }
 }
 
 fn main() {
@@ -23,25 +33,22 @@ fn main() {
                 .join("dyn-dns-update.conf")
             ).expect("Could not read settings");
     let settings: Settings = serde_json::from_str(&settings).expect("Could not extract settings");
-    let log_settings = Settings {
-        passwd: "***".to_string(),
-        ..settings
-    };
-    println!("Settings: {log_settings:#?}");
+    println!("Settings: {settings:#?}");
 
     let public_ip = get_public_ip().expect("Could not get public ip");
     println!("body = {public_ip:#?}", );
 
+    let is_up_to_date = |resolved_domain: &ResolvedDomain| {
+        let res = resolved_domain.ips.contains(&public_ip);
+        if res == true { println!("{} already up to date", resolved_domain.name); }
+        res
+    };
 
-    let hostname = "familie.uriegel.de";
-
-    let ips: Vec<IpAddr> = lookup_host(hostname).unwrap();
-    println!("dns lookup {hostname}: {ips:?}");    
-
-    let hostname = "uriegel.de";
-
-    let ips: Vec<IpAddr> = lookup_host(hostname).unwrap();
-    println!("dns lookup {hostname}: {ips:?}");    
+    let domains_to_refresh: Vec<ResolvedDomain> = settings.domains
+        .iter()
+        .map(resolve_domain)
+        .filter(is_up_to_date)
+        .collect();
 }
 
 fn get_public_ip() -> Result<IpAddr, reqwest::Error> {
@@ -52,3 +59,18 @@ fn get_public_ip() -> Result<IpAddr, reqwest::Error> {
     let ipstr = ipstr[.. pos].trim();
     Ok(IpAddr::from_str(ipstr).unwrap())
 }
+
+struct ResolvedDomain {
+    ips: Vec<IpAddr>,
+    name: String
+}
+
+fn resolve_domain(domain: &String) -> ResolvedDomain {
+    let ips = lookup_host(domain).unwrap();
+    println!("dns lookup {domain}: {ips:?}");    
+    ResolvedDomain {
+        ips,
+        name: domain.to_string()
+    }
+}
+
