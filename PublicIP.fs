05278fc4419d15e781ebc4ff5fa86df1d3b798ac
Uwe Riegel <uriegel@hotmail.de>
2022-11-22 20:56:06 +0100

    PublicIP is repeated up to 3 times on error

--- PublicIP.fs
+++ PublicIP.fs
@@ -3,15 +3,18 @@ module PublicIP
 open FSharpTools
 open FSharpHttpRequest
 open AsyncResult
+open System
 
 let get () = 
-    let extractIp = 
-        String.subStringBetweenStrs "Address: " "<"
-        >> (Option.defaultValue "")
-        >> Async.toAsync
+    let get () = 
+        let extractIp = 
+            String.subStringBetweenStrs "Address: " "<"
+            >> (Option.defaultValue "")
+            >> Async.toAsync
 
-    Request.getString { Request.defaultSettings with Url = "http://checkip.dyndns.org" }
-    |> mapError Error.fromIpResult
-    |>> extractIp
+        Request.getString { Request.defaultSettings with Url = "http://checkip.dyndns.org" }
+        |> mapError Error.fromIpResult
+        |>> extractIp
     
-
+    get
+    |> repeatOnError (TimeSpan.FromSeconds 5) 4 
