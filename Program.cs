557a2ce750c2e26a571baa6c486424d419d041a9
Uwe Riegel <uriegel@hotmail.de>
2024-03-25 21:35:44 +0100

    Finished

--- Program.cs
+++ Program.cs
@@ -1,34 +1,21 @@
-﻿using static System.Console;
+﻿using CsTools.Async;
+using CsTools.Extensions;
+using CsTools.Functional;
+using static System.Console;
 
 WriteLine("Starting Dyn DNS Auto Updater...");
 
-WriteLine(await PublicIP.Get());
+var ip = await PublicIP.Get();
+WriteLine($"Outer IP address: {ip}");
 
-WriteLine(Settings.Get());
+var results = await
+    Settings
+        .Get()
+        .Domains
+        .Select(d => Update.Run(Settings.Get(), ip, d))
+        .ToAsyncResults()
+        .Collect();
+
+WriteLine("Dyn DNS Auto Updater finished");
+results.ForEach(n => n.Match(s => { }, e => WriteLine($"Error: {e}")));
        
-//     let performUpdate = 
-//         let rec performUpdate res ip ipList = async {
-//             match ipList, res with
-//             | [] , _ -> return res 
-//             | _ , Error _ -> return res 
-//             | head::tail, _ -> 
-//                 let! res = Update.update (getSettings ()) head ip
-//                 return! performUpdate res ip tail          
-//         }
-        
-//         performUpdate (Ok ()) 
-
-//     let runAll ip = 
-//         (getSettings ())
-//             .Domains  
-//             |> Array.toList
-//             |> performUpdate ip
-
-//     match! PublicIP.get () >>= runAll with
-//     | Error err -> 
-//         printfn "Dyn DNS Auto Updater finished: %O" err
-//     | _ -> 
-//         printfn "Dyn DNS Auto Updater finished"
-// } 
-// |> Async.RunSynchronously
-
--- Update.cs
+++ Update.cs
@@ -2,15 +2,25 @@ using CsTools.Extensions;
 using CsTools.Functional;
 using CsTools.HttpRequest;
 
+using static System.Console;
+using static CsTools.Core;
 using static CsTools.HttpRequest.Core;
+using static CsTools.Functional.AsyncResultExtensions;
 
 static class Update 
 {
-    public static AsyncResult<string, RequestError> Run(Settings settings, string ip, string domain)   
-        // await Dns.Check(domain, ip)
-        // ? Unit.Value.SideEffect(_ => WriteLine($"{domain}: IP Address not changed, no action needed")).ToAsync()
-        // : Unit.Value.ToAsync();
+    public static AsyncResult<string, RequestError> Run(Settings settings, string ip, string domain)
+        => RepeatOnError(() => RunOnce(settings, ip, domain), 4, TimeSpan.FromSeconds(5)); 
+    
+    static AsyncResult<string, RequestError> RunOnce(Settings settings, string ip, string domain)   
+        => Dns.Check(domain, ip)
+         ? Ok<string, RequestError>("")
+            .SideEffect(_ => WriteLine($"{domain}: IP Address not changed, no action needed"))
+            .ToAsync()
+            .ToAsyncResult()
+         : UpdateRequest(settings, ip, domain);
 
+    static AsyncResult<string, RequestError> UpdateRequest(Settings settings, string ip, string domain)   
         => Request
             .Run(DefaultSettings with
                 {
@@ -19,41 +29,8 @@ static class Update
                     Headers = [
                         new Header("User-Agent", "DynDNS Updater"),
                         BasicAuthentication.From("uriegel.de", "juliachiara1")], 
-                    Url = $"/nic/update?hostname={domain}&myip={ip}"
+                    Url = $"/nic/update?hostname={domain.SideEffect(d => WriteLine($"Updating {d}"))}&myip={ip}"
                 })
-            .BindAwait(m => m.ReadAsStringAwait());
+            .BindAwait(m => m.ReadAsStringAwait())
+            .SideEffectWhenOk(s => WriteLine($"Response: {s}"));
 }
-// let updateOnce settings host ip = 
-
-
-//     let mapResult (resStr: string) = 
-//         printfn "response: %s" resStr 
-//         () 
-//         |> Async.toAsync
-
-//     let printUrl url = printfn "Updating %s" url
-
-//     match DnsCheck.check host ip with
-//     | false ->
-//         Request.getString { 
-//             Request.defaultSettings with 
-//                 Url = (getUrl () ) |> sideEffect printUrl
-//                 Headers = Some [| 
-//                     { Key = "User-Agent"; Value = "DynDNS Updater" }
-//                     BasicAuthentication.addHeader settings.Account settings.Passwd
-//                 |]  
-//             }
-//             |> mapError Error.fromUpdateResult
-//             |>> mapResult
-
-//     | true ->
-//         printfn "%s: IP Address not changed, no action needed" host
-//         printfn ""
-//         Ok () 
-//         |> Async.toAsync
-
-// let update settings host ip = 
-//     let funToRun () = updateOnce settings host ip 
-    
-//     funToRun
-//     |> repeatOnError (TimeSpan.FromSeconds 5) 4 
