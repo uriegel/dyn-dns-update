ec07edd26c97d67907896322e6c99bc373d102d5
Uwe Riegel <uriegel@hotmail.de>
2026-05-22 13:04:37 +0200

    .NET 10

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using CsTools.Async;
-using CsTools.Extensions;
 using CsTools.Functional;
 using static System.Console;
 
@@ -8,14 +7,23 @@ WriteLine("Starting Dyn DNS Auto Updater...");
 var ip = await PublicIP.Get();
 WriteLine($"Outer IP address: {ip}");
 
-var results = await
+var tasks =
     Settings
         .Get()
         .Domains
-        .Select(d => Update.Run(Settings.Get(), ip, d))
-        .ToAsyncResults()
-        .Collect();
+        .Select(d => Update.Run(Settings.Get(), ip, d));
+foreach (var task in tasks)
+{
+    try
+    {
+        await task;
+    }
+    catch(Exception e)
+    {
+        WriteLine($"Error: {e}");
+    }
+}
 
 WriteLine("Dyn DNS Auto Updater finished");
-results.ForEach(n => n.Match(s => { }, e => WriteLine($"Error: {e}")));
+
        
--- Update.cs
+++ Update.cs
@@ -5,32 +5,34 @@ using CsTools.HttpRequest;
 using static System.Console;
 using static CsTools.Core;
 using static CsTools.HttpRequest.Core;
-using static CsTools.Functional.AsyncResultExtensions;
 
 static class Update 
 {
-    public static AsyncResult<string, RequestError> Run(Settings settings, string ip, string domain)
-        => RepeatOnError(() => RunOnce(settings, ip, domain), 4, TimeSpan.FromSeconds(5)); 
-    
-    static AsyncResult<string, RequestError> RunOnce(Settings settings, string ip, string domain)   
-        => Dns.Check(domain, ip)
-         ? Ok<string, RequestError>("")
-            .SideEffect(_ => WriteLine($"{domain}: IP Address not changed, no action needed"))
-            .ToAsync()
-            .ToAsyncResult()
-         : UpdateRequest(settings, ip, domain);
-
-    static AsyncResult<string, RequestError> UpdateRequest(Settings settings, string ip, string domain)   
-        => Request
-            .Run(DefaultSettings with
-                {
-                    Method = HttpMethod.Get,
-                    BaseUrl = $"https://{settings.Provider}",
-                    Headers = [
+    public static Task<string> Run(Settings settings, string ip, string domain)
+        => RepeatOnError(async () =>
+        {
+            if (Dns.Check(domain, ip))
+            {
+                WriteLine($"{domain}: IP Address not changed, no action needed");
+                return "";
+            }
+            else
+                return await UpdateRequest(settings, ip, domain);
+        }, new RepeatOnErrorOptions() {RepeatCount = 4, WaitTime = TimeSpan.FromSeconds(5) });
+            
+    static async Task<string> UpdateRequest(Settings settings, string ip, string domain)
+    {
+        var response = await Request.RunAsync(DefaultSettings with
+        {
+            Method = HttpMethod.Get,
+            BaseUrl = $"https://{settings.Provider}",
+            Headers = [
                         new Header("User-Agent", "DynDNS Updater"),
-                        BasicAuthentication.From(settings.Account, settings.Passwd)], 
-                    Url = $"/nic/update?hostname={domain.SideEffect(d => WriteLine($"Updating {d}"))}&myip={ip}"
-                })
-            .BindAwait(m => m.ReadAsStringAwait())
-            .SideEffectWhenOk(s => WriteLine($"Response: {s}"));
+                        BasicAuthentication.From(settings.Account, settings.Passwd)],
+            Url = $"/nic/update?hostname={domain.SideEffect(d => WriteLine($"Updating {d}"))}&myip={ip}"
+        });
+        var result = await response.Content.ReadAsStringAsync();
+        WriteLine($"Response: {result}");
+        return result;
+    }
 }
